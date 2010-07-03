6cacc56bdc000b321995b324f810e764ffd052d4
Jacob Rothstein <github@jacobrothstein.com>
2010-07-03 01:14:27 -0700

    [ updated example ]

--- example.js
+++ example.js
@@ -2,9 +2,13 @@ var sys = require("sys");
 
 var puts = sys.puts;
 
-/* This takes the square */
-square = (function(x) {
-  return x * x
+var _globalVar_ = 1;
+
+square = (function(x) { 
+  // x:required
+  /* This takes the square */
+  
+  return x * x;
 });
 
 puts(square(3));
@@ -12,16 +16,61 @@ puts(square(3));
 (function() {
   var a = 6,
     b = 4;
-  
-  return puts(a + b)
+  puts(b - a);
+  return puts(a + b);
 })();
 
-puts(a(b, c));
+puts(["a", "b", "c"]);
 
-/* This takes the square */
-varargs = (function(&rest, a) {
-  return puts(a)
+varargs = (function(a, b, c, d, e, remainder) { 
+  // a:required b:optional c:required d:optional e:required remainder:rest
+  if (arguments.length < 5) // if b is missing
+    var e = d, d = c, c = b, b = undefined;
+  if (arguments.length < 4) // if d is missing
+    var e = d, d = undefined;
+  if (arguments.length < 3)
+    throw new Error("argument count mismatch: expected no fewer than 3 arguments");
+  var remainder = Array.prototype.slice.call(arguments, 5);
+  
+  return puts("a:" + a + "\n" +
+    "b:" + b + "\n" +
+    "c:" + c + "\n" +
+    "d:" + d + "\n" +
+    "e:" + e + "\n" +
+    "remainder:" + remainder);
 });
 
-varargs(a, b, c);
+varargs("a", "c", "e");
 
+varargs("a", "b", "c", "e");
+
+varargs("a", "b", "c", "d", "e");
+
+varargs("a", "b", "c", "d", "e", "f", "g");
+
+random = (function() { 
+  if (arguments.length > 0)
+    throw new Error("argument count mismatch: expected no arguments");
+  
+  return Math.random();
+});
+
+first = (function(arr) { 
+  // arr:required
+  return (arr)[0];
+});
+
+(function() {
+  var a = ["a", "b", "c", 1, "helloWorld"];
+  puts(a.slice(2, 3));
+  puts(10 - 7);
+  return 3 + 5;
+})();
+
+if (true) {
+  puts(10 - 7);
+  puts(2 + 5);
+  return puts((2 === 1 + 1));
+} else {
+  1 + 1
+}
