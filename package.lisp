1e101ac72f4bf8badd80ed35c60d5dff44fbde36
Jacob Rothstein <github@jacobrothstein.com>
2010-07-04 22:12:19 -0700

    [ package.lisp and package.json ]

---
+++ package.lisp
@@ -0,0 +1,13 @@
+( hash 'name 'sibilant
+      'version "0.0.1"
+      'keywords '(lisp javascript language)
+      'description "javascript with a lisp"
+      'contributors (hash 'name "Jacob Rothstein"
+      			  'web "http://jacobrothstein.com")
+      'repositories (hash 'type 'git
+      			  'url "http://github.com/jbr/sibilant.js")
+      'bugs (hash 'web "http://github.com/jbr/sibilant.js/issues")
+      'bin (hash 'sibilant "./bin/sibilant")
+      'directories (hash 'lib "./lib")
+      'main "./lib/sibilant")
+
