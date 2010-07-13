f6f25022a8bdaa3dbed3a5f236e8a22b6302a566
Jacob Rothstein <github@jacobrothstein.com>
2010-07-13 00:33:56 -0700

    [ rewrite functional.js in sibilant ]
    o the eval at the end indicates a need for quasiquotation
    o detect could be written better

---
+++ lib/functional.lisp
@@ -0,0 +1,46 @@
+(defvar functional exports)
+
+(defun inject (start items fn)
+  (defvar value start)
+  (when (array? items)
+      (dolist items
+	(lambda (item index)
+	  (setf value (fn value item index)))))
+  value)
+
+(defun map (items fn)
+  (inject (list) items
+	  (lambda (collector item index)
+	    (send collector push (fn item index))
+	    collector)))
+
+(defun select (items fn)
+  (inject (list) items
+	  (lambda (collector item index)
+	    (when (fn item index)
+	      (send collector push item))
+	    collector)))
+
+(defun detect (items fn)
+  (defvar return-item)
+  (defvar index 0)
+  (until (or (= items.length) return-item)
+    (when (fn (get item index) index)
+      (setf return-item (get item index)))
+    (incr index)))
+
+(defun reject (items fn)
+  (defvar args (list items fn))
+  (select items (lambda () (not (apply fn args)))))
+
+(defun compact (arr)
+  (select arr (lambda (item) (bool item))))
+
+(dolist '(inject map select detect reject compact)
+  (lambda (export-function)
+    (set exports export-function
+	 (eval export-function))))
+		  
+     
+
+		      
