323f119f056be82f68326a2bfbc194253c2d2544
Jacob Rothstein <github@jacobrothstein.com>
2010-08-24 00:34:54 -0700

    [ tidy up package file for npm ]

--- package.lisp
+++ package.lisp
@@ -3,11 +3,13 @@
       'keywords '(lisp javascript language)
       'description "javascript with a lisp"
       'contributors (hash 'name "Jacob Rothstein"
-      			  'web "http://jacobrothstein.com")
+      			  'web  "http://jacobrothstein.com")
       'repositories (hash 'type 'git
-      			  'url "http://github.com/jbr/sibilant")
+      			  'url  "http://github.com/jbr/sibilant")
       'bugs (hash 'web "http://github.com/jbr/sibilant/issues")
       'bin (hash 'sibilant "./bin/sibilant")
-      'directories (hash 'lib "./lib")
+      'modules (hash 'repl       "./lib/repl"
+		     'functional "./lib/functional"
+		     'import     "./lib/import")
       'main "./lib/sibilant")
 
