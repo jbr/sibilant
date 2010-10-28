dc380614f5c97893b2f00223672c63b9bdf9542f
Jacob Rothstein <github@jacobrothstein.com>
2010-10-28 00:00:50 -0700

    [ remove examples because they're out of date ]

--- examples/server.lisp
+++
@@ -1,8 +0,0 @@
-(defvar http (require 'http))
-
-(defvar server (http.create-server (lambda (request, response)
-  (response.write-head 200 (hash "Content-Type" "text/plain"))
-  (response.end "Hello World\n"))))
-
-(server.listen 8124 "127.0.0.1")
-(console.log "Server running at http://127.0.0.1:8124/")
