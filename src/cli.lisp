9596d8f896d785d2bac502301c0e1b2aba440dae
Jacob Rothstein <github@jacobrothstein.com>
2010-10-29 00:30:28 -0700

    [ add a fairly verbose --help ]

--- src/cli.lisp
+++ src/cli.lisp
@@ -17,7 +17,60 @@
 (defun cli.repl (&rest args)
   (require "sibilant/repl"))
 
+(defun cli.help (&rest args)
+  (console.log
+"Hi there!  Thanks for installing sibilant.
+Please leave feedback on github issues (http://github.com/jbr/sibilant/issues)
+
+The current commandline options are:
+--------------------------------------------------------------------------------
+
+-h / --help            This message
+
+--repl / [no args]     Sibilant interactive command prompt
+
+--execute / -x         This is a flag. Execute input files in order supplied.
+
+--output DIR / -o DIR  Output input files to this dir, replacing .lisp with .js.
+
+--input FILE / -i FILE / FILE
+                       Add this file to the input files. If the execute flag is
+                       set, input files will be executed.  If an output dir is
+                       specified, each file will be written to that dir.
+                       Otherwise, each file will be written to STDOUT.
+
+--------------------------------------------------------------------------------
+
+Examples
+
+to compile sibilant
+$ git clone git://github.com/jbr/sibilant.git
+$ npm link .
+$ sibilant src/*.lisp -o lib
+$ sibilant -x test/test.lisp # you're now running a sibilant you just compiled.
+
+to compile one file to stdout
+$ sibilant test/test.lisp
+
+to compile a file to a directory
+$ sibilant test/test.lisp -o . # put test.js here
+or
+$ sibilant --input test/test.lisp --output .
+
+to run a file
+$ sibilant -x test/test.lisp
+
+to enter the repl
+$ sibilant
+or
+$ sibilant --repl
+
+"))
+
 (defvar cli-options (options cli))
+
+(when (empty? (keys cli-options))
+  (cli.repl))
 
 (defvar output-dir
   (when cli-options.output (first cli-options.output)))
@@ -38,6 +91,7 @@
   (set context '**dirname (path.dirname input-path))
   (set module 'filename input-path)
   (script.run-in-context js context 'sibilant))
+
 
 (each (input-file) (or cli-options.input (list))
       (defvar input-path (path.join (process.cwd) input-file)
