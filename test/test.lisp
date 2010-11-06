5435c8eb2ddd85e8e7d9acd3b62d4df32c213a95
Jacob Rothstein <github@jacobrothstein.com>
2010-11-06 01:12:58 -0700

    [ additional testing apparatus ]
    At some point this should be a macro that shadows a function.
    
    (defun    assert-equal (expected actual &optional message) ...)
    (defmacro assert-equal (expected actual)
    	(defvar message ..something-fancy-here..)
    	(call assert-equal expected actual message))
    
    Where the macro constructs the message by examining the expected and actual before translating them.  For example, (assert-equal 1 (foo bar)) could say something like "expected (foo bar) to be 1 but was 0"

--- test/test.lisp
+++ test/test.lisp
@@ -9,14 +9,22 @@
 (defun trim (string)
   (send string trim))
 
-(defun assert-equal (expected actual)
+(defun assert-equal (expected actual &optional message)
   (sys.print (if (= expected actual) "."
-	       (concat "F\n\nexpected: " expected
-		       "\n\nbut got: " actual "\n\n"))))
+	       (concat "F\n\n" (if message (concat message "\n\n") "") "expected "expected 
+		        "\n\nbut got " actual "\n\n"))))
 
 (defun assert-translation (sibilant-code js-code)
   (assert-equal (trim js-code)
 		(trim (sibilant.translate-all sibilant-code))))
+
+(defun assert-true (&rest args)
+  (args.unshift true)
+  (apply assert-equal args))
+
+(defun assert-false (&rest args)
+  (args.unshift false)
+  (apply assert-equal args))
 
 
 (assert-translation "5"        "5")
