9ca4e3e31d9b8d8ac03ba1c90fc4e1935d142f9b
Jacob Rothstein <github@jacobrothstein.com>
2010-11-07 22:28:54 -0800

    [ simplify sibilant.info.lisp ]
    in addition to cleanup, this removes the partially-baked tab indentation.  textareas just aren't editors.  yet.

--- public/javascripts/sibilant.info.lisp
+++ public/javascripts/sibilant.info.lisp
@@ -55,55 +55,15 @@
                              (keyup)))))
 
 
-    (when (contains? window.location.hash (list "" "#"))
-    	(setf window.location.hash
-    	      (chain items (first) (attr 'id))))
-
-    (defun calculate-height ()
-	(chain ($ "textarea, #output")
-	       (each (lambda (&rest args)
-		       (defvar block ($ this)
-                               value (+ (send block val)
-					(send block text))
-                               lines (length
-				      (or (list)
-					  (send value match /[^\s]\n/g))))
-		       (block.height (* (+ 1 lines) 20))))))
+    (switch window.location.hash
+            (("" "#")
+             (setf window.location.hash
+                   (chain items (first) (attr 'id)))))
 
     (defvar textarea ($ 'textarea))
 
-    (defun determine-tab-stop (value)
-      (defvar lines       (send value split "\n")
-              open-parens (list))
-      (each (line) lines
-	  (each (char offset) (line.split "")
-                (switch char
-                        ("(" (open-parens.push offset))
-                        (")" (open-parens.pop)))))
-      (+ 2 (last open-parens)))
-
-    (defun repeat (times string)
-      (defvar return-buffer "")
-      (while (< 0 times)
-	(incr-by return-buffer string)
-	(setf times (- times 1)))
-      return-buffer)
-
     (chain textarea
 	   (focus)
-	   (keydown (lambda (evt)
-		      (when (contains? evt.key-code (list 13 9))
-			(defvar prev-value (chain textarea (val))
-                          tab "  "
-                          cursor-position (send textarea attr 'selection-start))
-
-			(when (<= prev-value.length cursor-position)
-			  (defvar tab
-			    (repeat (determine-tab-stop prev-value) " "))
-			  (when (= 13 evt.key-code)
-			    (setf tab (concat "\n" tab)))
-			  (send textarea val (concat prev-value tab)))
-			false)))
 	   (keyup (lambda (evt)
                     (defvar output ($ "#output"))
 		    (try (chain output
