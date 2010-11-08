a9380ff3093864e15e0f7e26ad087e56da972249
Jacob Rothstein <github@jacobrothstein.com>
2010-11-07 19:55:45 -0800

    [ update the public site's sibilant to be current ]

--- public/javascripts/sibilant.info.lisp
+++ public/javascripts/sibilant.info.lisp
@@ -5,53 +5,54 @@
   ((get macros "!=") -1 (macros.send (translate arr) 'index-of item)))
 
 
-(j-query (lambda (jq)
-    (defun check-hash (&rest ignored)
+($ (thunk
+    (defvar $window ($ window))
+
+    (defun check-hash ()
       (when (defined? check-hash.timeout)
 	(clear-timeout check-hash.timeout)
 	(delete check-hash.timeout))
+
       
-      (when (!= window.location.hash (send (jq window) data 'last-hash))
-	  (chain (jq window)
-	    (data 'last-hash window.location.hash)
-	    (trigger 'hash-change window.location.hash)))
-	(setf check-hash.timeout (set-timeout check-hash 500)))
+      (when (!= window.location.hash ($window.data 'last-hash))
+        (chain $window
+          (data 'last-hash window.location.hash)
+          (trigger 'hash-change window.location.hash)))
+
+      (setf check-hash.timeout (set-timeout check-hash 500)))
     
-    (defvar items (jq "script[language=sibilant/example]"))
+    (defvar items ($ "script[language=sibilant/example]"))
 
-    (send (jq window) click
-	  (lambda (evt)
-	    (set-timeout check-hash 25)))
+    ($window.click (thunk (set-timeout check-hash 25)))
 
-    (send (jq window) bind 'hash-change
-	  (lambda (evt hash)
-	    (defvar item (send items filter hash))
-	    (when (< 0 item.length)
-	      (defvar content (chain item
-				     (text)
-				     (replace "<![CDATA[\n", "")
-				     (replace "]]>" "")))
-	      (defvar title (send item attr "data-title"))
-	      (send (jq "header h2") html title)
+    ($window.bind 'hash-change
+                  (lambda (evt hash)
+                    (defvar item (send items filter hash))
+                    (when (< 0 item.length)
+                      (defvar content (chain item
+                                             (text)
+                                             (replace "<![CDATA[\n", "")
+                                             (replace "]]>" "")))
+                      (defvar title (send item attr "data-title"))
+                      (send ($ "header h2") html title)
+                      (defvar next (send item next items.selector))
+                      (defvar prev (send item prev items.selector))
+                      
+                      (if (> next.length 0)
+                          (chain ($ "#next")
+                                 (attr 'href (concat "#" (send next attr 'id)))
+                                 (show))
+                        (send ($ "#next") hide))
+                      
+                      (if (> prev.length 0)
+                          (chain ($ "#prev")
+                                 (attr 'href (concat "#" (send prev attr 'id)))
+                                 (show))
+                        (send ($ "#prev") hide))
 
-	      (defvar next (send item next items.selector))
-	      (defvar prev (send item prev items.selector))
-
-	      (if (> next.length 0)
-		  (chain (jq "#next")
-			 (attr 'href (concat "#" (send next attr 'id)))
-			 (show))
-		(send (jq "#next") hide))
-
-	      (if (> prev.length 0)
-		  (chain (jq "#prev")
-			 (attr 'href (concat "#" (send prev attr 'id)))
-			 (show))
-		(send (jq "#prev") hide))
-
-	      (chain (jq "textarea")
-		     (val (trim content))
-		     (keyup)))))
+                      (chain ($ "textarea")
+                             (val (trim content))
+                             (keyup)))))
 
 
     (when (contains? window.location.hash (list "" "#"))
@@ -59,28 +60,26 @@
     	      (chain items (first) (attr 'id))))
 
     (defun calculate-height ()
-	(chain (jq "textarea, #output")
+	(chain ($ "textarea, #output")
 	       (each (lambda (&rest args)
-		       (defvar block (jq this))
-		       (defvar value (+ (send block val)
-					(send block text)))
-		       (defvar lines (length
+		       (defvar block ($ this)
+                               value (+ (send block val)
+					(send block text))
+                               lines (length
 				      (or (list)
 					  (send value match /[^\s]\n/g))))
-		       (send block height
-			     (* (+ 1 lines) 20))))))
+		       (block.height (* (+ 1 lines) 20))))))
 
-    (defvar textarea (jq 'textarea))
+    (defvar textarea ($ 'textarea))
 
     (defun determine-tab-stop (value)
-      (defvar lines (send value split "\n"))
-      (defvar open-parens (list))
-      (dolist lines
-	(lambda (line)
-	  (dolist (send line split "")
-	    (lambda (char offset)
-	      (when (= "(" char) (send open-parens push offset))
-	      (when (= ")" char) (send open-parens pop))))))
+      (defvar lines       (send value split "\n")
+              open-parens (list))
+      (each (line) lines
+	  (each (char offset) (line.split "")
+                (switch char
+                        ("(" (open-parens.push offset))
+                        (")" (open-parens.pop)))))
       (+ 2 (last open-parens)))
 
     (defun repeat (times string)
@@ -94,10 +93,10 @@
 	   (focus)
 	   (keydown (lambda (evt)
 		      (when (contains? evt.key-code (list 13 9))
-			(defvar prev-value (chain textarea (val)))
-			(defvar tab "  ")
-			(defvar cursor-position
-			  (send textarea attr 'selection-start))
+			(defvar prev-value (chain textarea (val))
+                          tab "  "
+                          cursor-position (send textarea attr 'selection-start))
+
 			(when (<= prev-value.length cursor-position)
 			  (defvar tab
 			    (repeat (determine-tab-stop prev-value) " "))
@@ -106,11 +105,11 @@
 			  (send textarea val (concat prev-value tab)))
 			false)))
 	   (keyup (lambda (evt)
-		    (try (chain (jq "#output")
-				(text (sibilant.translate-all
-				       (send textarea val)))
+                    (defvar output ($ "#output"))
+		    (try (chain output
+				(text (sibilant.translate-all (textarea.val)))
 				(remove-class 'error))
-			 (chain (jq "#output")
+			 (chain output
 				(text e.stack)
 				(add-class 'error))))))
     (check-hash)))
