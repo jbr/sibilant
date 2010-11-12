59e3e183f1b4d6411d1ec066ff0a547e2b45849d
Jacob Rothstein <github@jacobrothstein.com>
2010-11-11 18:01:23 -0800

    [ bump version to 0.0.10 ]

--- package.lisp
+++ package.lisp
@@ -1,5 +1,5 @@
 (hash 'name 'sibilant
-      'version "0.0.9"
+      'version "0.0.10"
       'keywords '(lisp javascript language)
       'description "javascript with a lisp"
       'contributors (hash 'name "Jacob Rothstein"
