d6f6a79199da250d64e440fdc5f5f9c8a9004d75
Jacob Rothstein <github@jacobrothstein.com>
2010-12-05 22:45:46 -0800

    [ v0.0.11 ]
    Maybe it's almost time for 0.1.0.

--- package.lisp
+++ package.lisp
@@ -1,5 +1,5 @@
 (hash 'name 'sibilant
-      'version "0.0.10"
+      'version "0.0.11"
       'keywords '(lisp javascript language)
       'description "javascript with a lisp"
       'contributors (hash 'name "Jacob Rothstein"
