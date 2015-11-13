6d9b3e31a9eacc597bc5ec01fbd437a7ccafa097
Jacob Rothstein <hi@jbr.me>
2015-11-12 18:45:12 -0800

    update website to include imaginary filename
    better than eval.sibilant

--- public/javascripts/sibilant.info.js
+++ public/javascripts/sibilant.info.js
@@ -1,12 +1,12 @@
 //# sourceMappingURL=/javascripts/sibilant.info.map
 ;
 var lineCount = (function lineCount$(text) {
-  /* line-count /Users/jbr/code/sibilant/public/javascripts/sibilant.info.sibilant:5:0 */
+  /* line-count /Users/jbr/code/sibilant/public/javascripts/sibilant.info.sibilant:4:0 */
 
   return (text.split("\n"))["length"];
 });
 var resize = (function resize$(textarea, pre) {
-  /* resize /Users/jbr/code/sibilant/public/javascripts/sibilant.info.sibilant:7:0 */
+  /* resize /Users/jbr/code/sibilant/public/javascripts/sibilant.info.sibilant:6:0 */
 
   var lines = {
     textarea: lineCount(textarea.val()),
@@ -15,20 +15,20 @@ var resize = (function resize$(textarea, pre) {
   return $(textarea.add(pre)).css("height", ((Math.max(lines.textarea, lines.pre)) + "em"));
 });
 $((function() {
-  /* /Users/jbr/code/sibilant/public/javascripts/sibilant.info.sibilant:14:3 */
+  /* /Users/jbr/code/sibilant/public/javascripts/sibilant.info.sibilant:13:3 */
 
   return sibilant.loadMacros((function() {
-    /* /Users/jbr/code/sibilant/public/javascripts/sibilant.info.sibilant:16:5 */
+    /* /Users/jbr/code/sibilant/public/javascripts/sibilant.info.sibilant:15:5 */
   
     var trim = (function trim$(item) {
-      /* trim /Users/jbr/code/sibilant/public/javascripts/sibilant.info.sibilant:17:6 */
+      /* trim /Users/jbr/code/sibilant/public/javascripts/sibilant.info.sibilant:16:6 */
     
       return item.replace((new RegExp("^\\s*|\\s*$", "g")), "");
     });
     $("textarea").wrap("<div class=\"example\"/>")
-      .after($("<pre/>").text("sibilant"))
+      .after($("<pre/>"))
       .keyup((function() {
-        /* /Users/jbr/code/sibilant/public/javascripts/sibilant.info.sibilant:22:16 */
+        /* /Users/jbr/code/sibilant/public/javascripts/sibilant.info.sibilant:21:16 */
       
         var textarea = $(this),
             pre = textarea.siblings("pre");
@@ -37,6 +37,7 @@ $((function() {
             return textarea.val(trim(textarea.val()));
           }
         })();
+        (sibilant)["file"] = ("examples/" + textarea.attr("id") + ".sibilant");
         (function() {
           try {
             return $(pre).text(trim(sibilant.sibilize(textarea.val())))
@@ -50,7 +51,7 @@ $((function() {
       }))
       .trigger("keyup")
       .blur((function() {
-        /* /Users/jbr/code/sibilant/public/javascripts/sibilant.info.sibilant:38:15 */
+        /* /Users/jbr/code/sibilant/public/javascripts/sibilant.info.sibilant:39:15 */
       
         return $(this).trigger("keyup");
       }))
