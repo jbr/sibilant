de022c330082a0b0cd547dc999fa15f7253f342d
Jacob Rothstein <hi@jbr.me>
2015-12-19 01:38:52 -0800

    inline all core macros for browser version
    this way, all that’s required for a functional sibilant compiler is lib/browser.js
    
    also, move cardinal to dev dependencies for webpack convenience

--- public/javascripts/sibilant.info.js
+++ public/javascripts/sibilant.info.js
@@ -3,7 +3,7 @@
 var lineCount = (function lineCount$(text) {
   /* line-count /Users/jbr/code/sibilant/public/javascripts/sibilant.info.sibilant:4:0 */
 
-  return (text.split("\n"))["length"];
+  return text.split("\n").length;
 });
 var resize = (function resize$(textarea, pre) {
   /* resize /Users/jbr/code/sibilant/public/javascripts/sibilant.info.sibilant:6:0 */
@@ -17,45 +17,41 @@ var resize = (function resize$(textarea, pre) {
 $((function() {
   /* /Users/jbr/code/sibilant/public/javascripts/sibilant.info.sibilant:13:3 */
 
-  return sibilant.loadMacros((function() {
-    /* /Users/jbr/code/sibilant/public/javascripts/sibilant.info.sibilant:15:5 */
+  var trim = (function trim$(item) {
+    /* trim /Users/jbr/code/sibilant/public/javascripts/sibilant.info.sibilant:14:4 */
   
-    var trim = (function trim$(item) {
-      /* trim /Users/jbr/code/sibilant/public/javascripts/sibilant.info.sibilant:16:6 */
+    return item.replace((new RegExp("^\\s*|\\s*$", "g")), "");
+  });
+  $("textarea").wrap("<div class=\"example\"/>")
+    .after($("<pre/>"))
+    .keyup((function() {
+      /* /Users/jbr/code/sibilant/public/javascripts/sibilant.info.sibilant:19:14 */
     
-      return item.replace((new RegExp("^\\s*|\\s*$", "g")), "");
-    });
-    $("textarea").wrap("<div class=\"example\"/>")
-      .after($("<pre/>"))
-      .keyup((function() {
-        /* /Users/jbr/code/sibilant/public/javascripts/sibilant.info.sibilant:21:16 */
-      
-        var textarea = $(this),
-            pre = textarea.siblings("pre");
-        (function() {
-          if ((!(textarea.get(0) === document.activeElement))) {
-            return textarea.val(trim(textarea.val()));
-          }
-        })();
-        (sibilant)["file"] = ("examples/" + textarea.attr("id") + ".sibilant");
-        (function() {
-          try {
-            return $(pre).text(trim(sibilant.sibilize(textarea.val())))
-              .removeClass("error");
-          } catch (e) {
-            return $(pre).text(e.message)
-              .addClass("error");
-          }
-        })();
-        return resize(textarea, pre);
-      }))
-      .trigger("keyup")
-      .blur((function() {
-        /* /Users/jbr/code/sibilant/public/javascripts/sibilant.info.sibilant:39:15 */
-      
-        return $(this).trigger("keyup");
-      }))
-      .show();
-    return $("article").scrollNav({ titleText: null });
-  }));
+      var textarea = $(this),
+          pre = textarea.siblings("pre");
+      (function() {
+        if (!(textarea.get(0) === document.activeElement)) {
+          return textarea.val(trim(textarea.val()));
+        }
+      }).call(this);
+      sibilant.file = ("examples/" + textarea.attr("id") + ".sibilant");
+      (function() {
+        try {
+          return $(pre).text(trim(sibilant.sibilize(textarea.val())))
+            .removeClass("error");
+        } catch (e) {
+          return $(pre).text(e.message)
+            .addClass("error");
+        }
+      }).call(this);
+      return resize(textarea, pre);
+    }))
+    .trigger("keyup")
+    .blur((function() {
+      /* /Users/jbr/code/sibilant/public/javascripts/sibilant.info.sibilant:37:13 */
+    
+      return $(this).trigger("keyup");
+    }))
+    .show();
+  return $("article").scrollNav({ titleText: null });
 }));
